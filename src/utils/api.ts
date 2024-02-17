bc9c2296102044eaa0d18d46f46b7fed64a08a10
Kelechukwu Ikechukwu <obiefunalelechukwu@gmail.com>
2024-02-17 05:58:53 +0100

    feat: designed al screens with their logics

---
+++ src/utils/api.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance } from "axios";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+const instance: AxiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default instance;
